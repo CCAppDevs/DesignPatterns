5ed714a512951d0554f275a23e23cfd879382072
Jesse Harlan <jesse.harlan@centralia.edu>
2024-10-03 13:50:35 -0700

    Add project files.

---
+++ DesignPatterns/DBSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class DBSingleton
+    {
+        private static DBSingleton instance { get; set; }
+
+        public string ConnectionString { get; set; }
+
+        private DBSingleton(string connection)
+        {
+            ConnectionString = connection;
+        }
+
+        public DBSingleton getInstance(string connection)
+        {
+            if (instance == null)
+            {
+                instance = new DBSingleton(connection);
+            }
+
+            return instance;
+        }
+    }
+}
